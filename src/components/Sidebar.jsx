e3908bc9701056a39e72735c4fa8bebc01e5cbc5
Sudais Khan <sudais8075@gmail.com>
2025-12-11 18:12:46 +0500

    text added

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -140,7 +140,7 @@ export default function Sidebar() {
                   link.click();             
                 }}
               >
-                Download Resume
+                Download My CV
               </Button>
             </MotionBox>
 
